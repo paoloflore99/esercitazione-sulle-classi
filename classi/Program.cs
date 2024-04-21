14603daa2eabfcba002a67b4ae455d55fa129f3e
paolo flore <paoloflore937@gmail.com>
2024-04-21 17:58:46 +0200

    Aggiungere i file di progetto.

---
+++ classi/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace classi
+{
+    public class auto()
+    {
+        public int peso;
+
+
+
+        public class miaauto()
+        {
+
+        }
+    }
+
+    class ruota()
+    {
+        public string marca;
+    }
+
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           auto miaauto = new auto ();
+
+           
+        }
+    }
+}
